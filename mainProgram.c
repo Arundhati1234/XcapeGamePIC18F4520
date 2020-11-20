75ce6e1c50a6d49883230c24a0c528084088b67a
Arundhati Bandopadhyaya <38293989+Arundhati1234@users.noreply.github.com>
2020-11-20 23:31:38 +0530

    Final

--- mainProgram.c
+++ mainProgram.c
@@ -1,10 +1,20 @@
+/*
+ * File:   mainProgram.c
+ * Author: bandopadhyaya.arundhati.btech2018@sitpune.edu.in
+ *
+ * Created on 11/12/2020 2:57:31 AM UTC
+ * "Created in MPLAB Xpress"
+ */
+
 #include<pic18f4520.h>
 #include<xc.h>
 #include "check.c"
 
 void main(){
     
-    LCDinterface();   
+    LCDinterface(); 
+
+    
 }
 
 
